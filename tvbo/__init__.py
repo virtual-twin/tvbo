e27141bc6436426a4b168fd30d1263bd4b4650c6
leon-k-martin <leon.martin@bih-charite.de>
2025-11-20 18:15:42 +0100

    Release v0.2.6

--- tvbo/__init__.py
+++ tvbo/__init__.py
@@ -27,7 +27,7 @@ __authors__ = [
     "Petra Ritter",
 ]
 
-__version__ = "0.2.5"
+__version__ = "0.2.6"
 __maintainer__ = "Leon K. Martin (leon.martin@bih-charite.de)"
 __contact__ = "petra.ritter@charite.de"
 __status__ = "beta"
