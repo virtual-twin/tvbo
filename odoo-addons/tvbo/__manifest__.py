fc6617cf58bea1feccb5ec30e1f39c12371008f3
leon-k-martin <leon.martin@bih-charite.de>
2025-11-21 19:51:35 +0100

    refactor: Improve __manifest__.py structure and add missing dependencies

--- odoo-addons/tvbo/__manifest__.py
+++ odoo-addons/tvbo/__manifest__.py
@@ -1,2 +1,28 @@
 # -*- coding: utf-8 -*-
-{'name': 'TVBO - The Virtual Brain Ontology', 'version': '19.0.1.0.0', 'category': 'Research/Neuroscience', 'summary': 'Data models for brain simulation, DBS, and neuroanatomical atlases', 'description': '\n            Complete TVBO data models including:\n            - Brain simulation dynamics and experiments\n            - Deep Brain Stimulation (DBS) protocols\n            - Neuroanatomical atlases and parcellations\n        ', 'author': 'Charité Universitätsmedizin Berlin', 'website': 'https://github.com/virtual-twin/tvbo', 'license': 'EUPL-1.2', 'depends': ['base'], 'data': ['security/ir.model.access.csv'], 'installable': True, 'application': True, 'auto_install': False}
+{
+    'name': 'TVBO - The Virtual Brain Ontology',
+    'version': '19.0.1.0.0',
+    'category': 'Research/Neuroscience',
+    'summary': 'Data models for brain simulation, DBS, and neuroanatomical atlases',
+    'description': '''
+        Complete TVBO data models including:
+        - Brain simulation dynamics and experiments
+        - Deep Brain Stimulation (DBS) protocols
+        - Neuroanatomical atlases and parcellations
+    ''',
+    'author': 'Charité Universitätsmedizin Berlin',
+    'website': 'https://github.com/virtual-twin/tvbo',
+    'license': 'EUPL-1.2',
+    'depends': ['base', 'website'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menus.xml',
+        'views/database_views.xml',
+        'views/website_templates.xml',
+        'data/neural_models.xml',
+        'data/integrators.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
