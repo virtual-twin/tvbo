37e02475ec0c4dab89f1b010c53d97043220bd9f
leon-k-martin <leon.martin@bih-charite.de>
2026-03-13 13:28:08 +0100

    Refactor network initialization and update documentation references
    - Updated the network initialization in `docs/index.qmd` to use `Network.from_db` for better clarity and efficiency.
    - Adjusted the coupling assignment to reflect the new initialization method.
    - Revised API documentation links in `docs/reference.qmd` for accuracy and consistency.
    - Enhanced the prerender script to include a new script for updating replication TOC.

--- docs/scripts/prerender.sh
+++ docs/scripts/prerender.sh
@@ -11,6 +11,7 @@ set -e
 python scripts/tvbo_package_struct.py
 python -m quartodoc build --config api/_quartodoc_config.yml
 python scripts/update_toc_api.py
+python scripts/update_toc_replication.py
 python scripts/generate_datamodel_docs.py
 python scripts/update_toc_datamodel.py
 python scripts/fix_mermaid_blocks.py
