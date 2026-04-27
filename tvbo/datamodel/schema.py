257336e674a4be142da31d41cadfe118b12ce6ef
leon-k-martin <leon.martin@bih-charite.de>
2026-04-27 11:31:05 +0200

    feat: add class_uri to various data model classes in pydantic.py and update linkml_meta for consistency fix: update generation date in schema.py to reflect the latest changes

--- tvbo/datamodel/schema.py
+++ tvbo/datamodel/schema.py
@@ -1,5 +1,5 @@
 # Auto generated from tvbo_datamodel.yaml by pythongen.py version: 0.0.1
-# Generation date: 2026-04-16T16:27:51
+# Generation date: 2026-04-27T11:30:52
 # Schema: tvb-datamodel
 #
 # id: https://w3id.org/tvbo
