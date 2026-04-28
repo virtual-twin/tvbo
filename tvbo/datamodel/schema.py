122108f7076f098730061c7ddb98f0b6cc3f39f4
leon-k-martin <leon.martin@bih-charite.de>
2026-04-28 14:23:26 +0200

    fix: update generation date in schema.py

--- tvbo/datamodel/schema.py
+++ tvbo/datamodel/schema.py
@@ -1,5 +1,5 @@
 # Auto generated from tvbo_datamodel.yaml by pythongen.py version: 0.0.1
-# Generation date: 2026-04-28T13:47:12
+# Generation date: 2026-04-28T14:01:40
 # Schema: tvb-datamodel
 #
 # id: https://w3id.org/tvbo
