cdb8c84f34acc09d4dd4f53c6c66c3c16ec698e0
leon-k-martin <leon.martin@bih-charite.de>
2026-05-10 09:36:12 +0200

    bump version

--- tvbo/__init__.py
+++ tvbo/__init__.py
@@ -45,7 +45,7 @@ __authors__ = [
     "Petra Ritter",
 ]
 
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 __maintainer__ = "Leon K. Martin (leon.martin@bih-charite.de)"
 __contact__ = "petra.ritter@charite.de"
 __status__ = "beta"
