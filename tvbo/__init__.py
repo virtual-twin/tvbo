c08c592379b8e686cb64a5af8c7e68c034fb4a03
leon-k-martin <leon.martin@bih-charite.de>
2026-07-03 17:18:24 +0200

    chore: update version to 0.5.1 and release date in CITATION.cff and __init__.py

--- tvbo/__init__.py
+++ tvbo/__init__.py
@@ -45,7 +45,7 @@ __authors__ = [
     "Petra Ritter",
 ]
 
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 __maintainer__ = "Leon K. Martin (leon.martin@bih-charite.de)"
 __contact__ = "petra.ritter@charite.de"
 __status__ = "beta"
